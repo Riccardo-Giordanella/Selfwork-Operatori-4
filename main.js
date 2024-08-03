32d4a5c4f6a49c1c83018d731664a3a15f4ff656
Riccardo Giordanella <it.gg@hotmail.it>
2024-08-03 10:15:51 +0200

    Fine esercizio

---
+++ main.js
@@ -0,0 +1,18 @@
+// Per questo esercizio mi potrebbe aiutare una canzone in particolare :D
+let x = 44
+let y = 6
+const file = Math.round(x / y)
+const resto = x % y
+const mancanti = y - resto
+
+// Calcolo delle file
+console.log(`Le file sono:`, file);
+
+// Calcolo dei gatti mancanti per completare una nuova fila
+console.log(`I gatti mancanti per completare un'altra fila sono:`, mancanti);
+
+// Calcolo dei gatti che rimangono fuori
+
+console.log(`I gatti che rimangono fuori sono:`, resto);
+
+// Fine esercizio
